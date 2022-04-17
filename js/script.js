cfcd2deb3f69479b6fed18db3bd5d03fa6b9ada2
Eugen Avramenko <eugenavramenko234@gmail.com>
2022-04-17 11:03:22 +0300

    recommit

--- js/script.js
+++ js/script.js
@@ -1,77 +1,10 @@
-"use strict"
-
-let numberOfFims;
-
-function start () {
-   numberOfFims = +prompt ('Сколько фильмов вы уже посмотрели', '');
-
-   while (numberOfFims == '' || numberOfFims == null || isNaN(numberOfFims));
-   numberOfFims = +prompt ('Сколько фильмов вы уже посмотрели', '');
-}
-
-//start();
 
 
-let personalMovieDB = {
-   count: numberOfFims,
-   movies: {},
-   actors: {},
-   genres: [],
-   privat: false
-};
-
-function remberMyFilms () {
-   for (let i = 0;  i < 2; i++) {
-      const a = prompt('Один из последних просмотренных фильмов?'),
-            b = prompt( 'На сколько оцените его?');
-   
-   
-      if (a != null && b != null && a != '' && b !='' && a.lenth > 50) {
-         personalMovieDB.movies[a] = b;
-         console.log('done')
-      } else {
-         console.log('error');
-      }   
-   } 
-}
-
-//remberMyFilms();
-
-function detectPersonalLvl() {
-   if (personalMovieDB.count < 10) {
-      console.log('Просмотрено довольно мало фильмов');
-   } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-      console.log('Вы классический зритель');
-   } else if (personalMovieDB.count > 30) {
-      console.log ('Вы киноман');
-   } else {
-      console.log('Произошла ошибка');
-   }
-}
-
-//detectPersonalLvl ();
-
-function showMyDB (hidden) {
-   if (!hidden) {
-      console.log(personalMovieDB);
-   } 
-}
-
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres () {
-   for (let i = 1; i <= 3; i++) {
-      const genre = prompt(`Ваш любимый жанр ${i}`);
-      personalMovieDB.genres[i - 1] = genre; 
-   }
-}
-
-writeYourGenres();
 
 
-   
 
- 
+
+
 
 
 
